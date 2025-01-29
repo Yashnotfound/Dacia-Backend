a3978dabad587a903cdc915c660997bbfcf2a141
Yashnotfound <userongrid@Admins-MacBook-Pro-8.local>
2025-01-29 16:59:31 +0530

    Initialized project

---
+++ src/main/java/com/example/dacia/DaciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dacia/DaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
