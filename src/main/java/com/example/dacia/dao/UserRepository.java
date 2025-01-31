c012b0568a99a74b5422a6ad0cb5c737e1cd3707
Yashnotfound <userongrid@Admins-MacBook-Pro-8.local>
2025-01-31 22:53:57 +0530

    JWT Token Implemented fr fr ong

--- src/main/java/com/example/dacia/dao/UserRepository.java
+++ src/main/java/com/example/dacia/dao/UserRepository.java
@@ -4,8 +4,10 @@ import com.example.dacia.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     public boolean existsByEmail(String email);
-    public User findByEmail(String email);
+    public Optional<User> findByEmail(String email);
 }
--- src/main/java/com/example/dacia/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.dacia.service;
-
-import com.example.dacia.dto.request.UserLoginRequest;
-import com.example.dacia.dto.request.UserRegistrationRequest;
-
-public interface UserService {
-    String registerUser(UserRegistrationRequest userRegistrationRequest);
-    String loginUser(UserLoginRequest userLoginRequest);
-}
